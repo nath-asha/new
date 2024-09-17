9dbdf68d0d7bd3c85fd45220570fa09234ae5080
nath-asha <omthasha@gmail.com>
2024-09-17 19:06:11 +0530

    changed

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from '../app/components/Navbar';
+import Footer from '../app/components/Footer';
 
 export default function Home() {
   return (
